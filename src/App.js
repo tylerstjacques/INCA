f8808fdd07810df1d47fd72dcebb720f3a442e34
Tyler St Jacques <tylerstjacques@gmail.com>
2025-01-16 13:07:23 -0500

    Skeleton Layout of the Landing Page.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <img src={logo} className="App-logo" alt="logo" />
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>INCA</h1>
       </header>
+      <p>
+          This is the homepage of the INCA react app.
+      </p>
+      <div className="horizontal-layout">
+          <button className = "custom-button"
+            style={{
+              width: '100px',
+              padding: '20px 10px',
+              fontSize: '16px',
+              backgroundColor: 'blue',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}
+            onClick={() => alert('This Button Will Be Used to Come Back To Your Feed!')}
+          >
+          FEED
+          </button>
+          <button className = "custom-button"
+            style={{
+              width: '100px',
+              padding: '20px 10px',
+              fontSize: '16px',
+              backgroundColor: 'blue',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}
+            onClick={() => alert('This Button Will Be Used to Explore the Community!')}
+          >
+          EXPLORE
+          </button>
+          <button className = "custom-button"
+            style={{
+              width: '100px',
+              padding: '20px 10px',
+              fontSize: '16px',
+              backgroundColor: 'blue',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}
+            onClick={() => alert('This Button Will Be Used to Record An Activity!')}
+          >
+          RECORD
+          </button>
+          <button className = "custom-button"
+            style={{
+              width: '100px',
+              padding: '20px 10px',
+              fontSize: '16px',
+              backgroundColor: 'blue',
+              color: 'white',
+              border: 'none',
+              borderRadius: '5px',
+              cursor: 'pointer',
+            }}
+            onClick={() => alert('This Button Will Be Used to Go To Your Profile!')}
+          >
+          PROFILE
+          </button>
+        </div>
     </div>
   );
 }
